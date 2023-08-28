192319239323b53b477342ab124913f8a87d39e6
cgi-bin <s@ono.re>
2023-08-28 07:02:20 +0200

    push push

--- alephium.config.ts
+++ alephium.config.ts
@@ -14,13 +14,13 @@ const configuration: Configuration<Settings> = {
   networks: {
     devnet: {
       nodeUrl: 'http://127.0.0.1:22973',
-      privateKeys: [process.env.PRIVKEY_TESTNET],
+      privateKeys: process.env.PRIVATE_KEYS === undefined ? [] : process.env.PRIVKEY_TESTNET!.split(','),
       settings: defaultSettings
     },
 
     testnet: {
       nodeUrl: (process.env.NODE_URL as string) ?? 'https://wallet.testnet.alephium.org',
-      privateKeys: process.env.PRIVATE_KEYS === undefined ? [] : process.env.PRIVKEY_TESTNET.split(","),
+      privateKeys: process.env.PRIVATE_KEYS === undefined ? [] : process.env.PRIVKEY_TESTNET!.split(","),
       settings: defaultSettings
     },
 
