c89c169c7d1134c1fabe9839f37fd8cd3186236b
cgi-bin <s@ono.re>
2023-09-01 19:56:40 +0200

    small fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AlephiumWalletProvider } from '@alephium/web3-react'
-import { networkIds } from '@alephium/web3'
+import { NetworkId, networkIds } from '@alephium/web3'
+import { Network } from '@alephium/cli'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,8 +11,8 @@ export default function App({ Component, pageProps }: AppProps) {
       useTheme="minimal"
       network={
         process.env.NEXT_PUBLIC_NETWORK === undefined
-          ? 'mainnet'
-          : process.env.NEXT_PUBLIC_NETWORK
+          ? "mainnet" as NetworkId
+          : process.env.NEXT_PUBLIC_NETWORK as NetworkId
       }
     >
       <br />
