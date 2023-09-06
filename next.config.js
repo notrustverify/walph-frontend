fb27489f60980f2bdfd7832fb012149a0ba9a1f2
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2023-09-06 14:15:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false
