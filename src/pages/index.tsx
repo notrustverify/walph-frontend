5f76815e1b84cd82e05baad92e7629e7e5ddc14f
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2023-09-13 08:54:11 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,19 +23,16 @@ export default function Home() {
           <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
 
-          
-          <!-- Primary Meta Tags -->
           <meta name="title" content="Decentralized application to win ALPH, powered by No Trust Verify" />
           <meta name="description" content="Try your chance to win some ALPH by participating on the walph decentralized lottery, running on Alephium" />
           
-          <!-- Open Graph / Facebook -->
+
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://walph.io/" />
           <meta property="og:title" content="Decentralized application to win ALPH, powered by No Trust Verify" />
           <meta property="og:description" content="Try your chance to win some ALPH by participating on the walph decentralized lottery, running on Alephium" />
           <meta property="og:image" content="/images/apple-touch-icon.png" />
           
-          <!-- Twitter -->
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://walph.io/" />
           <meta property="twitter:title" content="Decentralized application to win ALPH, powered by No Trust Verify" />
