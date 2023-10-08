0675c7f1ae5be2213853903898d6b7b202357581
cgi-bin <s@ono.re>
2023-10-08 09:20:43 +0200

    chg:simplify buybutton label

--- src/components/BuyButtonLabel.tsx
+++ src/components/BuyButtonLabel.tsx
@@ -1,6 +1,4 @@
-import { WalphButton } from '@/services/walphTheme'
 import React from 'react'
-import Countdown from 'react-countdown'
 
 interface buyButtonLabel {
   slotFree: number
@@ -9,9 +7,7 @@ interface buyButtonLabel {
 
 export const BuyButtonLabel = ({ slotFree, count }: buyButtonLabel) => {
   
-    if(slotFree <= 0) return 'Walph full'
-    if (slotFree - count < 1) return 'Buy ' + count + ' ' + 'tickets'
-
+    if(slotFree <= 0) return <>Walph full</>
   
-  return 'Buy ' + count + ' ' + 'tickets'
+  return <>Buy {count} tickets</>
 }
--- src/pages/blitz.tsx
+++ src/pages/blitz.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import { AlephiumConnectButton, useWallet } from '@alephium/web3-react'
-import Link from 'next/link'
+import { useWallet } from '@alephium/web3-react'
 import { TimedWalph } from '@/components/TimedWalph'
-import { Walph50HodlAlf } from 'artifacts/ts'
 import { walphTheme } from "../services/walphTheme";
-import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
-import PoolSelector from '@/components/PoolSelector'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material/';
 import { Footer } from '@/components/Footer'
 import { HtmlHead } from '@/components/HtmlHead'
