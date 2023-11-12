0efa6cdead447d547e598a97fbb4d48ac99b6826
François Jolain <fjolain@gmail.com>
2023-11-12 19:00:00 +0100

    reset webapp with only react and mui. Setup frame with sidebar

---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+// MainContent.js
+import React from 'react';
+
+const MainContent = () => {
+    return (
+        <div>
+            {/* Your dynamic content goes here */}
+            <h1>Main Content</h1>
+            <p>This is where your dynamic content will be rendered.</p>
+        </div>
+    );
+};
+
+export default MainContent;
